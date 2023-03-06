07a3716b046a76e580defc62e47f9e9326834c01
Mathieu <cartron.mathieu37@gmail.com>
2023-03-06 11:52:47 +0100

    Update: test première fenetre

--- Main.cpp
+++ Main.cpp
@@ -5,8 +5,22 @@
 #include "Main.h"
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
 
 int main() {
+    sf::RenderWindow window(sf::VideoMode(640,480), "First Window!");
+
+    while(window.isOpen()) {
+        sf::Event event;
+        while(window.pollEvent(event)) {
+            if(event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.clear(sf::Color::Blue);
+        window.display();
+    }
     std::cout << "Hello, 2PROJ!" << std::endl;
     return 0;
 }
