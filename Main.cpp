e28c1be3f35f6405a5df76d10671ad970e04ba59
Mathieu <cartron.mathieu37@gmail.com>
2023-03-06 10:48:12 +0100

    first commit

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Mathieu on 3/6/2023.
+//
+
+#include "Main.h"
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, 2PROJ!" << std::endl;
+    return 0;
+}
---
+++ Main.h
@@ -0,0 +1,8 @@
+//
+// Created by Mathieu on 3/6/2023.
+//
+
+#ifndef TLWT_MAIN_H
+#define TLWT_MAIN_H
+
+#endif //TLWT_MAIN_H
