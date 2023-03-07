3e96332a61119adfd7a1e7f9c2c4e3d033a795e6
bilel233 <47856884+bilel233@users.noreply.github.com>
2023-03-07 19:30:39 +0100

    Update map.cpp
    creation d'une partie de map

--- map.cpp
+++ map.cpp
@@ -1,14 +1,17 @@
 #include <SFML/Graphics.hpp>
+#define CASE 20
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800,600),"Tower Defense");
 
-    int map[120][120] = {{0,1,1},{1,1,1},{0,0,1}};
+    sf::RenderWindow window(sf::VideoMode(800,600),"Tower Defense"); 
+
+    int map[20][20] = {{0,1,1},{1,1,1},{0,0,1},{1,1,1,1,0}};
 
     sf::RectangleShape rectangle(sf::Vector2f(120,120));
     rectangle.setFillColor(sf::Color(102,51,51));
-
+    rectangle.setOutlineThickness(3);
+    rectangle.setOutlineColor(sf::Color::White);
     while(window.isOpen())
     {
         sf::Event event;
@@ -19,11 +22,11 @@ int main()
         }
         window.clear();
 
-        for(int i=0;i<120;i++)
+        for(int i=0;i<20;i++)
         {
-            for(int j=0;j<120;j++)
+            for(int j=0;j<20;j++)
             {
-                rectangle.setPosition(i,j);
+                rectangle.setPosition(i*CASE,j*CASE);
                 window.draw(rectangle);
             }
         }
@@ -31,3 +34,4 @@ int main()
     }
    return 0;
 }
+
